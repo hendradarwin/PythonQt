6d88ce7972caed5a08522f402bfbcb1322293c7f
hendradarwin <hendra.darwintha@gmail.com>
2015-02-04 15:13:25 +0700

    Add Qt5.3.2 Support MSVC 2013 64 bit

--- src/PythonQtPythonInclude.h
+++ src/PythonQtPythonInclude.h
@@ -1,34 +1,34 @@
 /*
- *
- *  Copyright (C) 2011 MeVis Medical Solutions AG All Rights Reserved.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  Further, this software is distributed without any warranty that it is
- *  free of the rightful claim of any third person regarding infringement
- *  or the like.  Any license provided herein, whether implied or
- *  otherwise, applies only to this software file.  Patent licenses, if
- *  any, provided herein do not apply to combinations of this program with
- *  other software, or any other product whatsoever.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Contact information: MeVis Medical Solutions AG, Universitaetsallee 29,
- *  28359 Bremen, Germany or:
- *
- *  http://www.mevis.de
- *
- */
+*
+*  Copyright (C) 2011 MeVis Medical Solutions AG All Rights Reserved.
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  Further, this software is distributed without any warranty that it is
+*  free of the rightful claim of any third person regarding infringement
+*  or the like.  Any license provided herein, whether implied or
+*  otherwise, applies only to this software file.  Patent licenses, if
+*  any, provided herein do not apply to combinations of this program with
+*  other software, or any other product whatsoever.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*  Contact information: MeVis Medical Solutions AG, Universitaetsallee 29,
+*  28359 Bremen, Germany or:
+*
+*  http://www.mevis.de
+*
+*/
 
 //----------------------------------------------------------------------------------
 /*!
@@ -93,22 +93,22 @@
 #include <Python.h>
 
 #ifdef PYTHONQT_UNDEF_DEBUG
-# define _DEBUG
+# undef _DEBUG
 #endif
 
 /*
- * The following undefs for C standard library macros prevent
- * build errors of the following type on mac ox 10.7.4 and XCode 4.3.3
- *
+* The following undefs for C standard library macros prevent
+* build errors of the following type on mac ox 10.7.4 and XCode 4.3.3
+*
 /usr/include/c++/4.2.1/bits/localefwd.h:57:21: error: too many arguments provided to function-like macro invocation
-    isspace(_CharT, const locale&);
-                    ^
+isspace(_CharT, const locale&);
+^
 /usr/include/c++/4.2.1/bits/localefwd.h:56:5: error: 'inline' can only appear on functions
-    inline bool
-    ^
+inline bool
+^
 /usr/include/c++/4.2.1/bits/localefwd.h:57:5: error: variable 'isspace' declared as a template
-    isspace(_CharT, const locale&);
-    ^
+isspace(_CharT, const locale&);
+^
 */
 #undef isspace
 #undef isupper
@@ -119,4 +119,3 @@
 #undef tolower
 
 #endif
-
